2684a1c33190f22bd40af54649da60cfbe3bcb1a
whyanacarol <121807018+whyanacarol@users.noreply.github.com>
2025-05-19 14:17:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+    let botao = document.getElementById("cliqueaqui");
+    let mensagem = document.getElementById("mensagem");
+
+    botao.onclick = function () {
+        mensagem.innerText = "💜Não deixe o medo te dominar, você consegue!💜";
+    };
+
